3cf1931c06f54d7b42dce566f8fb605a2cb8fa22
Vlad Pavlichuk <exoit2014@vr-protect.email>
2022-12-08 19:33:45 +0300

    feat(init): Настройка prettier \ husky

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 type AppProps = {};
 
 const App: FC<AppProps> = () => {
-    return <h1>App</h1>;
+  return <h1>App</h1>;
 };
 
 export default App;
