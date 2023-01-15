e3aa445f5bd733b7891d0b261cf78adedd752fdf
Vlad Pavlichuk <exoit2014@vr-protect.email>
2023-01-15 17:37:12 +0300

    feat(homework): #12 Третье задание

--- src/components/hw-3/secondLvl-3.ts
+++ src/components/hw-3/secondLvl-3.ts
@@ -1,5 +1,5 @@
-type FIXME = any;
-const orderStates = ['initial', 'inWork', 'buyingSupplies', 'producing', 'fullfilled'] as const;
+type FIXME = Array<Omit<OrderState, 'buyingSupplies' | 'producing'>>;
+const orderStates = ['initial', 'inWork', 'buyingSupplies', 'producing', 'fulfilled'] as const;
 type OrderState = typeof orderStates[number];
 // Hint: type guards
 export const getUserOrderStates = (orderStates: OrderState[]): FIXME =>
